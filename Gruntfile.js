e1f0324a85bb12ba1d4abcab975506c37e83b59c
Airyque Ervin <airyque@gmail.com>
2017-04-28 22:12:49 -0700

    created a grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    nodemon: {
+      dev: {
+        script: './server/server.js'
+      }
+    },
+
+    shell: {
+        multiple: {
+          command: [
+            'sudo mongod',
+            'npm run build:watch',
+            'grunt nodemon'
+          ].join('&')
+        }
+      }
+
+
+  });
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('start', [
+    'shell'
+    ]
+  );
+};
