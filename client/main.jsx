a03cf096f944b81ee3e5182a2ebbd48da10c0218
Airyque Ervin <airyque@gmail.com>
2017-04-28 17:37:38 -0700

    Fixed client side error

--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,8 @@
-import { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-class Main extends Component {
+class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
