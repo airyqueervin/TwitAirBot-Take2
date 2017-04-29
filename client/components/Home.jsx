0075a468b8f10c0db87463a97f3b6990f1801ff1
Airyque Ervin <airyque@gmail.com>
2017-04-29 11:19:30 -0700

    Able to get value from input field and buttons.

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,16 +3,23 @@ import React from 'react';
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {value: ''};
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.runBot = this.runBot.bind(this);
     this.stopBot = this.stopBot.bind(this);
     this.sendTweet = this.sendTweet.bind(this);
   }
 
-  handleSubmit() {
+  handleChange(e) {
     console.log('Submit is being handled');
+    this.setState({value: e.target.value});
+  }
+
+  handleSubmit(e) {
+    alert(`A message was submitted: ${this.state.value}`);
+    e.preventDefault();
   }
 
   runBot() {
@@ -20,7 +27,7 @@ class Home extends React.Component {
   }
 
   sendTweet(val) {
-    alert(`This message is set: ${val}`);
+    alert(`This message was sent: ${val}`);
   }
 
   stopBot() {
@@ -33,15 +40,15 @@ class Home extends React.Component {
     return (
       <div>
        <h2>Live Home Component</h2>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           What do you want your bot to say?<br/>          
-          <input type="text" placeholder="Enter A Friednly Tweet" onChange={this.handleSubmit} />
+          <input type="text" placeholder="Enter A Friednly Tweet" value={this.state.value} onChange={this.handleChange} />
           <input type="submit"/>
         </form>
         <br/>
-        <button onClick={ this.sendTweet }>Send Single Tweet</button>
-        <button onClick={ this.stopBot }>Stop Bot</button>
-        <button onClick={ this.runBot }>Start Auto Tweet</button>
+        <button onClick={ () => {this.sendTweet(this.state.value)} }>Send Single Tweet</button>
+        <button onClick={this.stopBot}>Stop Bot</button>
+        <button onClick={this.runBot}>Start Auto Tweet</button>
       </div>
     );
   }
